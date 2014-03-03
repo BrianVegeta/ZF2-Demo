49695f8727b5c6c0d9382de2893a14ae0de98ea1
vegeta <vegeta@gmail.com>
2014-03-03 16:19:04 +0800

    Add service config for album table

--- module/Album/Module.php
+++ module/Album/Module.php
@@ -13,6 +13,11 @@ use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\Mvc\ModuleRouteListener;
 use Zend\Mvc\MvcEvent;
 
+use Album\Model\Album;
+use Album\Model\AlbumTable;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
+
 class Module implements AutoloaderProviderInterface
 {
     public function getAutoloaderConfig()
@@ -33,6 +38,25 @@ class Module implements AutoloaderProviderInterface
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
+    }
+    
+    public function getServiceConfig()
+    {
+    	return array(
+			'factories' => array(
+    			'Album\Model\AlbumTable' =>  function($sm) {
+    				$tableGateway = $sm->get('AlbumTableGateway');
+    				$table = new AlbumTable($tableGateway);
+    				return $table;
+    			},
+    			'AlbumTableGateway' => function ($sm) {
+    				$dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
+    				$resultSetPrototype = new ResultSet();
+    				$resultSetPrototype->setArrayObjectPrototype(new Album());
+    				return new TableGateway('album', $dbAdapter, null, $resultSetPrototype);
+    			},
+			),
+    	);
     }
 
     public function onBootstrap(MvcEvent $e)
